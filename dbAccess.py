d20d4ae80bbec903e5104a7d135310b74eccfb7d
Bradley <bst28@uakron.edu>
2024-04-13 12:04:53 -0700

    Validate and create dbs

--- dbAccess.py
+++ dbAccess.py
@@ -1,53 +1,17 @@
 import sqlite3
 from sqlite3 import Error
 
+from pdb import set_trace
+
 import apsw
 import apsw.bestpractice
+
+apsw.bestpractice.apply(apsw.bestpractice.recommended)
 
 import os
 
 deckFile = r'db\decklists.db'
 magicInfo = r'db\magicinfo.db'
-
-conn = None
-
-try:
-    #can pas :memory: to make the db be stored in ram
-    conn = sqlite3.connect(deckFile)
-    print(sqlite3.version)
-except Error as e:
-    print(e)
-finally:
-    if conn:
-        conn.close()
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    try:
-        return sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return None
-
-def create_table(conn, name, table_params):
-    try:
-        c = conn.cursor()
-        c.execute('''CREATE TABLE {0}({1}) '''.format(name, table_params))
-        c.close()
-    except Error as e:
-        print(e)
-
-def add_deck_to_table(conn, vals):
-    c = conn.cursor()
-    sql = ''' INSERT INTO decks (name,price,avg_cmc,instants,sorcerys,lands,creatures,artifacts,enchantments,plainswakers,white,blue,black,red,green)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-    c.execute(sql, vals)
-    conn.commit()
 
 def check_table(conn, table_name):
     c = conn.cursor()
@@ -60,23 +24,23 @@ CREATE TABLE collection (
     collection_id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     cmc INTEGER NOT NULL,
-    power INTEGER,
-    toughness INTEGER,
-    set TEXT NOT NULL,
+    set_name TEXT NOT NULL,
     num INTEGER NOT NULL,
     type TEXT NOT NULL,
-    oracle TEXT,
     rarity TEXT NOT NULL,
     price REAL NOT NULL,
+    power INTEGER,
+    toughness INTEGER,
+    oracle TEXT,
     location INTEGER DEFAULT 1,
     loc_details TEXT,
     FOREIGN KEY (location)
-    REFERENCES locations (location_id)
+    REFERENCES locations(location_id)
        ON UPDATE SET DEFAULT
        ON DELETE SET DEFAULT
-    image BLOB,
 )
 '''
+
 DECKLIST_TABLE='''
 CREATE TABLE REPLACE_ME (
     name TEXT NOT NULL,
@@ -181,27 +145,32 @@ def search_for_db(path:str):
 class database:
     connection = None
 
-    def __init__(self, path):
+    def __init__(self, path=''):
         self.dbPath = path
 
         if not self.validateDb():
             self.createNewDB()
 
-        raise Exception(f"Invalid Path: Path provided is neither a valid file or folder.\n {self.dbPath}")
 
     def createNewDB(self):
         '''Create a new database with the path that was provided.'''
-
         # check if the given path is a dir
-        if os.path.isdir(self.dbPath):
-            os.mkdir(self.dbPath+'\\db\\')
-            self.dbPath += '\\db\\collection.db'
+        if not os.path.isdir(self.dbPath):
+            folder='db\\'
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+            self.dbPath = folder +'collection.db'
+        else:
+            self.dbPath = 'collection.db'
 
         self.connection = apsw.Connection(self.dbPath)
+        self.connection.pragma("foreign_keys", True)
         cursor = self.connection.cursor()
         cursor.execute(LOCATIONS_TABLE)
         cursor.execute(COLLECTION_TABLE)
         cursor.execute(DECKS_TABLE)
+
+        print("Created new database at:", self.dbPath)
 
     def validateDb(self):
         '''Validate the provided path a properly formatted .db file.
@@ -217,13 +186,19 @@ class database:
 
         # test if the db contains the correct tables
         self.connection = apsw.Connection(self.dbPath)
+        check = self.connection.pragma("integrity_check")
+        if check != "ok":
+            raise Exception("Database Integrity Error:", check)
+
         c = self.connection.cursor()
         reqs = ['locations', 'collection', 'decks']
         for req in reqs:
             tabeQuery=f"SELECT name FROM sqlite_master WHERE type='table' AND name='{req}';"
             value = c.execute(tabeQuery).fetchone()
             if value == []:
-                raise Exception(f"Invalid Data Base: The data base provided is not formatted correctly.\n {self.dbPath}")
+                raise Exception(f"Invalid DataBase: The database provided is not formatted correctly.\n {self.dbPath}")
+
+        print("Database at: ", self.dbPath, "VALIDATED")
         return True
 
     def addLocation(self, location):
@@ -258,10 +233,9 @@ class database:
         pass
 
     def add_to_deck(self, card):
-
         pass
 
-    def remove_card(self, name:str, set:str, num:int, count:int, location:str, foilType='nonfoil'):
+    def remove_card(self, card):
         """Remove a number of cards from the collection, if the count hits 0 the
         entire entry will be deleted.
 
@@ -279,6 +253,7 @@ class database:
     def update_card(self, name):
         # is this neccessary?
         pass
+
     def get_card(self):
         pass
 
@@ -293,10 +268,8 @@ class database:
         :return: all the locations cards are stored at
         """
         pass
+
 if __name__ == "__main__":
-    # sql_query = """SELECT name FROM sqlite_master"""
-    sql_query = """SELECT * FROM Krenko""" #
-    conn = create_connection(magicInfo)
-    c = conn.cursor()
-    c.execute(sql_query)
-    print(c.fetchall())
+
+    db = database('db\\collection.db')
+
